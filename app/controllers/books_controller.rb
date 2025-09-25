6b0867b94fbdb5539b5fe5ab0983fda735e24cb9
WataruKanzaki <224184043+zaki4615@users.noreply.github.com>
2025-09-25 09:57:55 +0000

    9/25 finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+    def new
+      @book = Book.new  
+    end  
+
+    def create
+     @user = current_user
+     @book = Book.new(book_params)
+     @book.user_id = current_user.id
+     @book.save
+     
+     redirect_to books_path
+    end
+
+    def index
+    end
+
+    def show
+    end
+  
+ 
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+ 
+end
+
