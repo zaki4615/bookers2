c3b9e85d9b06ec8f2e166f911342a3862772b56d
Ubuntu <ubuntu@ip-172-31-29-45.ap-northeast-1.compute.internal>
2025-10-02 03:38:09 +0000

    top about page  導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  get "home/about" => "homes#about", as: 'about'
+
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :update, :edit]
+  
 end
