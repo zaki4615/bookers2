f23e47851a64eba41c6ae791df7b3013ca9458d9
Ubuntu <ubuntu@ip-172-31-29-45.ap-northeast-1.compute.internal>
2025-10-02 07:51:17 +0000

    レイアウト  導入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def create
@@ -15,6 +16,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @new_book = Book.new
+    @books = Book.all
   end
 
   def destroy
